f2e6bd714567d2e6373933df133cd2f78b181c9c
Aden Beckitt <2946724+patriot1889@users.noreply.github.com>
2024-05-16 12:29:13 +0100

    Update sensor.py
    add lock (okkyfgfs)

--- custom_components/tuya_ble/sensor.py
+++ custom_components/tuya_ble/sensor.py
@@ -149,7 +149,7 @@ mapping: dict[str, TuyaBLECategorySensorMapping] = {
     "szjqr": TuyaBLECategorySensorMapping(
         products={
             **dict.fromkeys(
-                ["3yqdo5yt", "xhf790if"],  # CubeTouch 1s and II
+                ["3yqdo5yt", "xhf790if", "okkyfgfs"],  # CubeTouch 1s and II
                 [
                     TuyaBLESensorMapping(
                         dp_id=7,
