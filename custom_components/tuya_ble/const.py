f7456300c28a992e02a2e217197dc13e605efbba
Aden Beckitt <2946724+patriot1889@users.noreply.github.com>
2024-05-26 01:30:04 +0100

    Update const.py
    Fix strenum error

--- custom_components/tuya_ble/const.py
+++ custom_components/tuya_ble/const.py
@@ -2,7 +2,7 @@
 from __future__ import annotations
 from dataclasses import dataclass
 
-from homeassistant.backports.enum import StrEnum
+from enum import StrEnum
 from tuya_iot import TuyaCloudOpenAPIEndpoint
 from typing_extensions import Final
 
